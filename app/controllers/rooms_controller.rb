7dc52fa39facbb4218dc739bc3c2216ffa83bf27
RikuTakayama <ec2-user@ip-172-31-24-155.ap-northeast-1.compute.internal>
2024-11-27 10:14:16 +0000

    [CREATE]senior DM機能

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,10 @@
 class RoomsController < ApplicationController
-  
+
   def show
     @room = Room.find(params[:id])
-  end 
+    @messages= @room.messages.all
+    @message= Message.new
+    @young_user= @room.young_user
+    @senior_user= @room.senior_user
+  end
 end
